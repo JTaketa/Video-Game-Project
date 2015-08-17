018ef00887ecd10515349c54e41c20749ca5940f
JTaketa <draktak@gmail.com>
2015-08-16 22:07:31 -0700

    Create characterStatsconfig.lua

---
+++ characterStatsconfig.lua
@@ -0,0 +1,12 @@
+local characterStatsConfig = {}
+
+characterStatsConfig.hp = 100
+characterStatsConfig.level = 1
+characterStatsConfig.exp = 0
+characterStatsConfig.nextLevel = 100
+characterStatsConfig.attack = 10
+characterStatsConfig.defense = 10
+characterStatsConfig.special = 10
+characterStatsConfig.evade = 5
+
+return characterStatsConfig
