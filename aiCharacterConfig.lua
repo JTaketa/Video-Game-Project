ac488e66a7889db481e7d1a6bb2d75d39418f3c9
JTaketa <draktak@gmail.com>
2015-08-16 22:08:59 -0700

    Create aiCharacterConfig.lua

---
+++ aiCharacterConfig.lua
@@ -0,0 +1,10 @@
+local aiCharacterConfig = {}
+
+aiCharacterConfig.hp = 150
+aiCharacterConfig.level = 5
+aiCharacterConfig.attack = 15
+aiCharacterConfig.defense = 10
+aiCharacterConfig.special = 12
+aiCharacterConfig.evade = 6
+
+return aiCharacterConfig
